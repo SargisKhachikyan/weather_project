c643c2306df118af7c74be6905b15ca81e0adbc1
saqo <saqo@saqos-MacBook-Air.local>
2026-09-15 20:34:53 +0400

    changed full ui,Update weather screen, add dynamic backgrounds, and clean up UI code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_project/firebase_options.dart';
@@ -13,14 +12,6 @@ Future<void> main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  final messaging = FirebaseMessaging.instance;
-
-  final settings = await messaging.requestPermission(
-    alert: true,
-    badge: true,
-    sound: true,
   );
 
   final notificationsService = NotificationsService();
